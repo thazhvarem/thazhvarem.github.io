a36a021bc41833652cd1933ce61838b5fe0070ca
thazhvarem <154018095+thazhvarem@users.noreply.github.com>
2023-12-17 01:54:48 +0530

    update

---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+    <title>TVA Belt</title>
+    <!-- <script>
+        window.onload = function() {
+        var image1=document.getElementById("img1");
+        var image2=document.getElementById("img2");
+        var image3=document.getElementById("img3");
+        var image4=document.getElementById("img4");
+        var image5=document.getElementById("img5");
+        var image6=document.getElementById("img6");
+        var image7=document.getElementById("img7");
+        var img_array=["am.jpg","an.jpg","cr.jpg","h.jpg","r.jpg","t.jpg","v.jpg"];
+        var index=0;
+        var interval = 1000;
+    function slide() {
+        image1.src = img_array[index++%img_array.length];
+        image2.src = img_array[index++%img_array.length];
+        image3.src = img_array[index++%img_array.length];
+        image4.src = img_array[index++%img_array.length];
+        image5.src = img_array[index++%img_array.length];
+        image6.src = img_array[index++%img_array.length];
+        image7.src = img_array[index++%img_array.length];
+    }
+
+    setInterval(slide, interval);
+        }
+        </script> -->
+</head>
+<center>
+<body bgcolor="red">
+    <marquee scrollamount="20" loop hspace="2" vspace="3">
+        <?php for($i=0;$i<1000;$i++){
+            echo "<img src='an.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='am.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='h.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='cr.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='r.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='t.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='v.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='vs.jpg' width='400' height='400' name='image'/>";
+            echo "<img src='p.jpg' width='400' height='400' name='image'/>";
+        
+        }?>
+        <!-- <img id="img1" src="an.jpg"  width="400" height="400" name="image"/>
+        <img id="img2" src="am.jpg"  width="400" height="400" name="image"/>
+        <img id="img3" src="h.jpg"  width="400" height="400" name="image"/>
+        <img id="img4" src="cr.jpg"  width="400" height="400" name="image"/>
+        <img id="img5" src="r.jpg"  width="400" height="400" name="image"/>
+        <img id="img6" src="t.jpg"  width="400" height="400" name="image"/>
+        <img id="img7" src="v.jpg"  width="400" height="400" name="image"/>
+        <img id="img7" src="vs.jpg"  width="400" height="400" name="image"/>
+        <img id="img7" src="p.jpg"  width="400" height="400" name="image"/> -->
+    </marquee>
+    <!-- <marquee scrollamount="20"> -->
+        <h1 style="font-size:1200%;text-align:center;color:yellow;">
+    OOMBIKOoo...!</h1>
+    <!-- </marquee> -->
+</center>
+</body></html>
